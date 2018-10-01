936e1fb3b2428a30c9b454527fd3f4fac32704f6
gorplex <gorplexthefallen@gmail.com>
2018-10-01 14:35:28 -0700

    created example 1 and cleaned up ex0

--- examples/ex0_BlinkLED.py
+++ examples/ex0_BlinkLED.py
@@ -2,22 +2,22 @@ import boardfuncs
 import time
 
 #define pin names to use
-pin1 = "PB12"
-pin2 = "PC13"
+LEDPin1 = "PB12"
+LEDPin2 = "PC13"
 
 
 #setup board comunication
 bf = boardfuncs.BoardFuncs()
 
 #set LED pins to output mode (general purpose input output push pull)
-bf.setUpPin(pin1, boardfuncs.GPIO_PP)
-bf.setUpPin(pin2, boardfuncs.GPIO_PP)
+bf.setUpPin(LEDPin1, boardfuncs.GPIO_PP)
+bf.setUpPin(LEDPin2, boardfuncs.GPIO_PP)
 
 #print to user
 print("Blinking LED")
 while True:
     #toggle both pins
-    bf.togglePin(pin1)
-    bf.togglePin(pin2)
+    bf.togglePin(LEDPin1)
+    bf.togglePin(LEDPin2)
     #wait 1/2 second
     time.sleep(.5)
---
+++ examples/ex1_FadeLED.py
@@ -0,0 +1,43 @@
+import boardfuncs
+import time
+
+#define pin names to use
+LEDPin1 = "PB12"
+LEDPin2 = "PC13"
+PWMPin1 = "PA1"
+
+#setup board comunication
+bf = boardfuncs.BoardFuncs()
+
+#set LED pins to inputs
+bf.setPinMode(LEDPin1, bf.GPIO_IN)
+bf.setPinMode(LEDPin2, bf.GPIO_IN)
+
+#init PWM pin clock freq (Hz)
+bf.initPWM(testPin, 1000)
+
+#print to user
+print("Fading LED")
+i=10000
+while True:
+    while i > 0:
+        #set duty cycle of PWM pin 0-10000
+        bf.setPWM(PWMPin1, i)
+        #delay for 1/10 of a second
+        time.sleep(.1)
+        i -= 1000
+    while i < 10000:
+    #set duty cycle of PWM pin 0-10000
+        bf.setPWM(PWMPin1, i)
+        #delay for 1/10 of a second
+        time.sleep(.1)
+        i += 1000
+
+        
+        
+        
+        
+        
+        
+        
+        
