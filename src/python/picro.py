20f18a73556338883f3e069165aff8e462c9049b
gorplex <gorplexthefallen@gmail.com>
2018-10-02 16:05:10 -0700

    picro changed to class based

--- src/python/picro.py
+++ src/python/picro.py
@@ -1,24 +1,25 @@
-import serialcom
 from boardfuncs import *
 
-serial = None
 
-def end():
-    #send stop board 
-    pass
 
-def run(setup, loop):
-    serial = serialcom.SerialCom()
+class Robot(BoardFuncs):
+    def __init__(self, term=None, baud=None):
+        super().__init__(term, baud)
     
-    try:
-        setup()
-        while True:
+    def end(self):
+        #send stop board 
+        pass
+    
+    def run(self, setup, loop):
+        try:
+            setup()
+            while True:
             loop()
-    except:
-        #note two or more ctr-c will skip clean-up code
-        end()
-        #re-raise original error
-        raise 
+        except:
+            #note two or more ctr-c will skip clean-up code
+            end()
+            #re-raise original error
+            raise 
 
 class Button():
     def __init__(self, pin):
