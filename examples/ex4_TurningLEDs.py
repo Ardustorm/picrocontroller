f97a0382d5b754f2b54667bf1b56f5faccf95604
gorplex <gorplexthefallen@gmail.com>
2018-10-13 10:57:06 -0700

    debuged ex4

--- examples/ex4_TurningLEDs.py
+++ examples/ex4_TurningLEDs.py
@@ -5,49 +5,62 @@ import time
 LEDPin1 = "PB12"
 LEDPin2 = "PC13"
 
-curentState = 0
+#def state vars
+currentState = 0
 doneTime = 0
 
-def setup():
+#def states of LEDS
+def straight():
+    myRobot.clearPin(LEDPin1)
+    myRobot.clearPin(LEDPin2)
+
+def right():
+    myRobot.setPin(LEDPin1)
+    myRobot.clearPin(LEDPin2)
     
+def left():
+    myRobot.clearPin(LEDPin1)
+    myRobot.setPin(LEDPin2)
+
+def stop():
+    myRobot.setPin(LEDPin1)
+    myRobot.setPin(LEDPin2)
+
+def setup():
     #set LED pins to inputs
     myRobot.setPinMode(LEDPin1, picro.GPIO_IN)
     myRobot.setPinMode(LEDPin2, picro.GPIO_IN)
     
     #print to user
-    print("Starting Turning State Machine")
-
-def straight():
-    myRobot.setPin(LEDPin1)
-    myRobot.setPin(LEDPin2)
-
-
-def right():
-    myRobot.clearPin(LEDPin1)
-    myRobot.setPin(LEDPin2)
-    
-def left():
-    myRobot.setPin(LEDPin1)
-    myRobot.clearPin(LEDPin2)
+    print("Running LED State Machine")
 
 def loop():
+    #declare varibles you will update
+    global doneTime
+    global currentState
+    
     #check if timer is done
     if(time.time() >= doneTime):
         
         #if the current state is even
-        if(currentState%2 == 0):
-            #go straight and set timer
+        if(currentState >= 9):
+            #if done stop
+            stop()
+        elif(currentState%2 == 0):
+            #go straight, set timer, and increment state
             straight()
             doneTime = time.time() + 3
-        elif(curentState == 1 or currentState == 7):
-            #go right and set timer
+            currentState += 1
+        elif(currentState == 1 or currentState == 7):
+            #go right, set timer, and increment state
             right()
             doneTime = time.time() + 1.5
-        elif(curentState == 3 or currentState == 5):
+            currentState += 1
+        elif(currentState == 3 or currentState == 5):
+            #go left, set timer, and increment state
             left()
             doneTime = time.time() + 1.5
-        #always increment state when timer runs out
-        currentState += 1
+            currentState += 1
 
 if __name__ == "__main__":
     myRobot = picro.Robot()
